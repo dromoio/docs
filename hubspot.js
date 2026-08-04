02eee3b0eab7d6ddb58ab6deeb0f18cad5ca783d
Zeph <paul.zeph@gmail.com>
2026-08-04 20:56:48 +0530

    Add HubSpot tracking

---
+++ hubspot.js
@@ -0,0 +1,9 @@
+if (!document.getElementById("hs-script-loader")) {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.id = "hs-script-loader";
+  script.async = true;
+  script.defer = true;
+  script.src = "//js.hs-scripts.com/44698404.js";
+  document.head.appendChild(script);
+}
